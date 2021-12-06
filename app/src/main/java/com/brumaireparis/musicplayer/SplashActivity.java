aeb3cf356d30688e709ff265b34a506da29e833c
BrumaireParis <hbulyy@gmail.com>
2021-12-06 17:52:15 +0800

    Initial SplashActivity

---
+++ app/src/main/java/com/brumaireparis/musicplayer/SplashActivity.java
@@ -0,0 +1,96 @@
+package com.brumaireparis.musicplayer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        changeStatusBarTextColor(this.getWindow(),true);
+
+        //插入到OnCreate()中
+        if(Build.VERSION.SDK_INT >= 21) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+        }
+        init();
+    }
+
+    int flag=0;
+
+    public void skipClick(View v)
+    {
+        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+
+        flag=1;
+        SplashActivity.this.finish();
+    }
+
+    private void init(){
+        /*TextView textView=findViewById(R.id.version);
+        try{
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(),0);
+            textView.setText("version:"+packageInfo.versionName);
+        }
+        catch (PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+            textView.setText("version");
+        }*/
+
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                if (flag != 1) {
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+
+                    SplashActivity.this.finish();
+                }
+            }
+        };
+        timer.schedule(timerTask,3000);
+    }
+
+    public void changeStatusBarTextColor(Window window, boolean isBlack) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View decor = window.getDecorView();
+            int flags;
+            if (isBlack) {
+                //更改文字颜色为深黑色
+                flags = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            }
+            else {
+                //更改文字颜色为浅色
+                flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            }
+            decor.setSystemUiVisibility(flags);
+        }
+    }
+}
