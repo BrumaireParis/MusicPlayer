cd68e95603aa573f7526f8cca436dcb99121a9f8
BrumaireParis <hbulyy@gmail.com>
2021-12-12 11:30:30 +0800

    Update Phonecall

--- app/src/main/java/com/brumaireparis/musicplayer/Phonecall.java
+++ app/src/main/java/com/brumaireparis/musicplayer/Phonecall.java
@@ -5,11 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
