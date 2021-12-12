ca37b1c8723b656f7fac94f3a26f38b2e9c06171
BrumaireParis <hbulyy@gmail.com>
2021-12-12 11:28:55 +0800

    Update Phonecall

---
+++ app/src/main/java/com/brumaireparis/musicplayer/Phonecall.java
@@ -0,0 +1,59 @@
+package com.brumaireparis.musicplayer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class Phonecall extends Activity {
+
+    public void changeStatusBarTextColor(Window window, boolean isBlack) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View decor = window.getDecorView();
+            int flags;
+            if (isBlack) {
+                //更改文字颜色为深黑色
+                flags = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            }
+            else {
+                //更改文字颜色为浅色
+                flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            }
+            decor.setSystemUiVisibility(flags);
+        }
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phonecall);
+
+        askPermissions();
+
+        changeStatusBarTextColor(this.getWindow(),true);
+
+        EditText phoneNum=findViewById(R.id.editTextPhone);
+        ImageButton imageButton = findViewById(R.id.callButton);
+        imageButton.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:"+phoneNum.getText()));
+            startActivity(intent);
+        });
+    }
+
+    protected void askPermissions(){
+        String[] permissions = {
+                "android.permission.CALL_PHONE"
+        };
+        int requestCode = 200;
+        requestPermissions(permissions,requestCode);
+    }
+}
